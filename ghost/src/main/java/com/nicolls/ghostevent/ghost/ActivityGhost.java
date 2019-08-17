5c4be36cafd4579a224c7d19ac87ca4a20fdd0e3
nicolls <851778509@qq.com>
2019-08-18 01:16:34 +0800

    cd

--- ghost/src/main/java/com/nicolls/ghostevent/ghost/ActivityGhost.java
+++ ghost/src/main/java/com/nicolls/ghostevent/ghost/ActivityGhost.java
@@ -6,6 +6,7 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 
+import com.nicolls.ghostevent.ghost.utils.GhostUtils;
 import com.nicolls.ghostevent.ghost.utils.LogUtil;
 import com.nicolls.ghostevent.ghost.view.GhostWebView;
 
@@ -37,8 +38,10 @@ public class ActivityGhost extends Ghost {
                 ghostWebView = new GhostWebView(activity.getApplicationContext());
                 ghostWebView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
                         ViewGroup.LayoutParams.MATCH_PARENT));
-//            ghostWebView.setAlpha(0.001f);//不能设置成0，设置成0，将收不到事件，只要还有值 就可以接收到事件
-//            ghostWebView.setTranslationX(2500);
+                ghostWebView.setAlpha(0.001f);//不能设置成0，设置成0，将收不到事件，只要还有值 就可以接收到事件
+                int translationX = GhostUtils.displayWidth < 1000 ? 2500 : (GhostUtils.displayWidth + 300);
+                LogUtil.d(TAG, "translationX " + translationX);
+                ghostWebView.setTranslationX(translationX);
                 viewGroup.addView(ghostWebView, 0);
                 ghostWebView.setGhostEventCallBack(ghostEventCallBack);
                 ghostWebView.start();
@@ -83,7 +86,7 @@ public class ActivityGhost extends Ghost {
 
     @Override
     public void test() {
-        LogUtil.d(TAG,"test");
+        LogUtil.d(TAG, "test");
         if (ghostWebView != null) {
             ghostWebView.test();
         }
